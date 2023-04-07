096bfa9ca4fad4866df8d6c36c1d1c2f77df98ea
Abdur Rofi <abdur.rofi@mtarget.co>
2023-04-07 09:46:32 +0700

    update: Quiz

--- pages/quiz/mental-health.js
+++ pages/quiz/mental-health.js
@@ -1,11 +1,30 @@
 import firebase_app from "@/src/firebase/config";
 import { getAuth } from "firebase/auth";
-import { useEffect } from "react";
+import { useEffect, useState } from "react";
+import quizData from "@/pages/quiz/data/quizData.json";
 import Swal from "sweetalert2";
+import TitlePage from "@/components/TitlePage";
 
 export default function MentalHealth() {
   const auth = getAuth(firebase_app);
   const user = auth.currentUser;
+  const [currentIndex, setCurrentIndex] = useState(0);
+  const currentQuestion = quizData.questions[currentIndex];
+
+  const handleAnswer = (answer) => {
+    // Move to the next question based on the answer chosen
+    if (answer === "yes") {
+      setCurrentIndex(currentQuestion.yes);
+    } else if (answer === "no") {
+      setCurrentIndex(currentQuestion.no);
+    } else {
+      // Handle invalid answer
+    }
+  };
+  const handlePrev = () => {
+    // Move to the previous question
+    setCurrentIndex(currentQuestion.prev);
+  };
   useEffect(() => {
     auth.onAuthStateChanged((authUser) => {
       if (!authUser) {
@@ -15,7 +34,7 @@ export default function MentalHealth() {
           showConfirmButton: true,
           timer: 2000,
           width: 350,
-          heightAuto: true
+          heightAuto: true,
         }).then(() => {
           // Redirect the user to the login page
           window.location.href = "/login";
@@ -23,5 +42,40 @@ export default function MentalHealth() {
       }
     });
   }, []);
-  return <></>;
+  console.log(currentIndex);
+  return (
+    <>
+      <div className="mx-4 my-5">
+        <div className="flex flex-col items-center justify-center h-screen">
+          <h1 className="text-4xl font-bold mb-8">Quiz</h1>
+          {currentQuestion && (
+            <div className="flex flex-col items-center">
+              <h2 className="text-2xl mb-4">{currentQuestion.question}</h2>
+              <button
+                className="bg-blue-500 hover:bg-blue-600 text-white font-bold py-2 px-4 rounded w-full"
+                onClick={() => handleAnswer("yes")}
+              >
+                Yes
+              </button>
+              <button
+                className="bg-blue-500 hover:bg-blue-600 text-white font-bold py-2 px-4 rounded mt-2 w-full"
+                onClick={() => handleAnswer("no")}
+              >
+                No
+              </button>
+              {currentIndex !== 0 && (
+                <button
+                  className="bg-gray-500 hover:bg-gray-600 text-white font-bold py-2 px-4 rounded mt-4 w-full"
+                  onClick={handlePrev}
+                  disabled={currentQuestion.prev === 0}
+                >
+                  Previous
+                </button>
+              )}
+            </div>
+          )}
+        </div>
+      </div>
+    </>
+  );
 }
