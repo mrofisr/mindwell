f2ca7694fdafcf4f606744ee4df8d0d4e2b499ea
Abdur Rofi <abdur.rofi@mtarget.co>
2023-05-01 07:32:51 +0700

    update: Design

--- pages/_app.js
+++ pages/_app.js
@@ -10,6 +10,14 @@ export default function App({ Component, pageProps }) {
     <>
       <Head>
         <title>MindWell - Better Mind, Better Life</title>
+        {/* <!-- link manifest.json --> */}
+        <link rel="manifest" href="/manifest.json" />
+        {/* <!-- this sets the color of url bar  --> */}
+        <meta name="theme-color" content="#f43f5e" />
+        {/* <!-- this sets logo in Apple smatphones. --> */}
+        <link rel="apple-touch-icon" href="/logo-96x96.png" />
+        {/* <!-- this sets the color of url bar in Apple smatphones --> */}
+        <meta name="apple-mobile-web-app-status-bar" content="#f43f5e" />
       </Head>
       <div className="flex flex-col">
         <div className="h-full bg-gray-100">
